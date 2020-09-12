c452db8ccd8ddf6de714740f7d739976119a7eb4
Risa Herdianto <aputra.zha@gmail.com>
2020-09-12 17:55:41 +0700

    initial

---
+++ lib/constants/assets.dart
@@ -0,0 +1,13 @@
+class Assets {
+  Assets._();
+
+  // splash screen assets
+  static const String appLogo = "assets/icons/ic_appicon.png";
+
+  // login screen assets
+  static const String carBackground = "assets/images/img_login.jpg";
+
+  // placeholder image
+  static const String placeholderImage = "assets/images/placeholder_image.png";
+
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,57 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate/config/environment_config.dart';
+import 'package:flutter_boilerplate/screens/home/home_detail_screen.dart';
+
+const double _fabDimension = 56.0;
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  ContainerTransitionType _transitionType = ContainerTransitionType.fade;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Main')),
+      body: Center(
+        child: Text('BASE_URL = ${EnvironmentConfig.BASE_URL}'),
+      ),
+      floatingActionButton: OpenContainer(
+        transitionType: _transitionType,
+        transitionDuration: const Duration(seconds: 3),
+        openBuilder: (BuildContext context, VoidCallback _) {
+          return const HomeDetailScreen();
+        },
+        closedElevation: 6.0,
+        closedShape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(_fabDimension / 2),
+          ),
+        ),
+        closedColor: Theme.of(context).colorScheme.secondary,
+        closedBuilder: (BuildContext context, VoidCallback openContainer) {
+          return SizedBox(
+            height: _fabDimension,
+            width: _fabDimension,
+            child: Center(
+              child: Icon(
+                Icons.add,
+                color: Theme.of(context).colorScheme.onSecondary,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
